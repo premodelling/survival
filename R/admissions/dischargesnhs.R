466675fe549b78410ce7ea22c39590815518d367
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-02 19:04:32 +0000

    Update R/admissions/dischargesnhs.R

--- R/admissions/dischargesnhs.R
+++ R/admissions/dischargesnhs.R
@@ -3,3 +3,29 @@
 # Created by: greyhypotheses
 # Created on: 02/01/2022
 
+DischargesNHS <- function () {
+
+  # the age groups
+  age_groups <- c('0-5', '6-17', '18-64', '65-84', '85+')
+
+  # data reader
+  get_ <- function (age_group) {
+    age_group
+    readxl::read_xlsx(path = 'data/Covid-Publication-13-08-2020.xlsx',
+                      sheet = str_glue('Discharges {age_group}'),
+                      range = 'F13:ER14')
+
+  }
+  items <- lapply(X = age_groups, FUN = get_)
+
+  # structuring
+  readings <- dplyr::bind_rows(items)
+  readings$age_group <- age_groups
+  readings <- tibble::column_to_rownames(readings, var = 'age_group')
+
+  # set dates as index
+  readings <- as.data.frame(t(readings))
+
+
+
+}
