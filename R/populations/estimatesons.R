09435231156f72bb4eecbb8465ccfefcb09c40f4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-02 21:40:42 +0000

    Update R/populations/estimatesons.R

--- R/populations/estimatesons.R
+++ R/populations/estimatesons.R
@@ -4,17 +4,23 @@
 # Created on: 02/01/2022
 
 
+#' EstimatesONS
+#'
+#' Reads the female &male population of England encoded in 'data/ukpopestimatesmid2020on2021geography.xls
+#'
 EstimatesONS <- function () {
 
+  # Sex
   sex <- c('Female', 'Male')
 
+  # Data reader
   get_ <- function (sex) {
 
     # read
     data <- readxl::read_xls(path = 'data/ukpopestimatesmid2020on2021geography.xls',
                              sheet = stringr::str_glue('MYE2 - {sex}s'), range = 'B8:CQ12')
 
-    # focus on series of interest
+    # focus on the series of interest
     data <- data %>%
       filter(Name == 'ENGLAND') %>%
       select(!c('Name', 'Geography', 'All ages'))
@@ -24,17 +30,21 @@ EstimatesONS <- function () {
     data <- tibble::column_to_rownames(data, var = 'sex')
 
     return(data)
-
   }
   items <- lapply(X = sex, FUN = get_)
 
-  # structuring
-  readings <- dplyr::bind_rows(items)
 
-  # traspose
+  # Structuring
+  readings <- dplyr::bind_rows(items)
   readings <- as.data.frame(t(readings))
+
+
+  # After transpoing the age fields become data frame indices.  Herein, the ages are assigned to a field
   readings$age <- row.names(readings)
   row.names(readings) <- NULL
 
+  return(readings)
 
 }
+
+
