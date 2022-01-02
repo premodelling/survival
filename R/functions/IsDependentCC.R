eca4641e7c487aab99fb9ac397363f12f9745ce2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-02 01:46:39 +0000

    Update R/functions/IsDependentCC.R

--- R/functions/IsDependentCC.R
+++ R/functions/IsDependentCC.R
@@ -1,5 +1,5 @@
 # Title     : IsDependentCC.R
-# Objective : Dependency analysis w.r.t. between categorical variables
+# Objective : Correlation analysis w.r.t. categorical variables
 # Created by: greyhypotheses
 # Created on: 01/01/2022
 
@@ -11,25 +11,21 @@ IsDependentCC <- function(variables, reference, frame) {
   #' @param reference: The categorial reference variable
   #' @param frame: The table of data
 
-  estimates <- data.table()
+  estimates <- data.frame()
   for (variable in variables) {
 
-    writeLines(paste0('\n\nCase: ', variable))
+    # contingency table
     frequencies <- table(frame[[variable]], frame[[reference]])
-    print(frequencies)
-
-    # Are the elements of the fields independent?
-    # Null Hypothesis: Independent elements
-    chisquared <- chisq.test(frequencies, simulate.p.value = FALSE, rescale.p = FALSE, B = 2000)
 
     # What is the degree of association between the elements of the fields?
     cramercinq <- rcompanion::cramerV(frequencies, y = NULL, ci = FALSE, conf = 0.95, type = 'bca',
                                       R = 1000, histogram = FALSE, digits = 4, bias.correct = TRUE)
 
     # Add the estimates to the table of estimates
-    calculations <- data.table(field = variable, cramerv = cramercinq)
-    estimates <- rbind(estimates, calculations)
+    estimates[1, variable] <- cramercinq
   }
+
+  row.names(estimates) <- reference
 
   return(estimates)
 
