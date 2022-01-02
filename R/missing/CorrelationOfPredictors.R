3d73178c607a9b15b7784d4c5c69c2b3aacdf2d3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-02 18:32:26 +0000

    Add R/missing/CorrelationOfPredictors.R

---
+++ R/missing/CorrelationOfPredictors.R
@@ -0,0 +1,33 @@
+# Title     : CorrelationOfPredictors.R
+# Objective : Correlation measires of categorical predictors
+# Created by: greyhypotheses
+# Created on: 02/01/2022
+
+
+source(file = 'R/functions/IsCorrelatedCC.R')
+
+
+#'
+#' @description determines the degree of correlation between predictors via Cramer's V
+#'
+#' @param predictors: the categorical predictors
+#' @param data:
+#'
+CorrelationOfPredictors <- function (predictors, data) {
+
+  # the correlation of each variable with all others
+  design <- mapply(FUN = IsCorrelatedCC, reference = predictors, MoreArgs = list(variables = predictors, frame = data))
+  measures <- dplyr::bind_rows(design)
+
+  # matrix graph
+  ggcorrplot::ggcorrplot(corr = measures,
+                         type = 'upper',
+                         ggtheme = ggplot2::theme_minimal(),
+                         colors = c('#E46726', 'white', 'black'),
+                         outline.color = 'white',
+                         lab = TRUE,
+                         lab_size = 3,
+                         lab_col = 'grey',
+                         digits = 2)
+
+}
