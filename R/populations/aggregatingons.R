5bbfaa9bea8343df9fc5ac6d522e08001e93ec0f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-02 21:55:19 +0000

    Update R/populations/aggregatingons.R

--- R/populations/aggregatingons.R
+++ R/populations/aggregatingons.R
@@ -1,10 +1,14 @@
 # Title     : aggregatingons.R
-# Objective : TODO
+# Objective : Aggregating ONS population values
 # Created by: greyhypotheses
 # Created on: 02/01/2022
 
 source(file = 'R/populations/estimatesons.R')
 
+#' AggregatingONS
+#'
+#' @description Determines the population numbers per age groups of interest
+#'
 AggregatingONS <- function () {
 
   # get the population data
@@ -22,6 +26,7 @@ AggregatingONS <- function () {
   ages <- data.frame(left = seq(0, 90, 10), right = seq(9, 99, 10))
   ages$age_group <- paste(ages$left, ages$right, sep = '-')
   ages[ages$age_group == '90-99', 'age_group'] <- '90+'
+  ages$age_group <- as.factor(ages$age_group)
   ages$group <- as.integer(row.names(ages))
 
 
@@ -29,7 +34,10 @@ AggregatingONS <- function () {
   readings <- dplyr::left_join(x = readings, y = ages[, c('age_group', 'group')], by = 'group')
 
 
-
-
+  # aggregate by age group
+  aggregates <- readings %>%
+    select(age_group, Female, Male) %>%
+    group_by(age_group) %>%
+    summarise(female = sum(Female), male = sum(Male))
 
 }
