b2d0e714b666ee6ba33b8f2930c965e385312b89
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-03 19:00:03 +0000

    Update R/patients/admissionsnhs.R

--- R/patients/admissionsnhs.R
+++ R/patients/admissionsnhs.R
@@ -1,5 +1,5 @@
 # Title     : admissionsnhs.R
-# Objective : NHS Source
+# Objective : England's NHS COVID Admissions
 # Created by: greyhypotheses
 # Created on: 02/01/2022
 
@@ -10,20 +10,27 @@ AdmissionsNHS <- function () {
 
   # data reader
   get_ <- function (age_group) {
-    age_group
-    readxl::read_xlsx(path = 'data/Covid-Publication-13-08-2020.xlsx',
-                      sheet = stringr::str_glue('Admissions {age_group}'),
-                      range = 'F13:ER14')
+
+    # read
+    data <- readxl::read_xlsx(path = 'data/Covid-Publication-13-08-2020.xlsx',
+                              sheet = stringr::str_glue('Admissions {age_group}'),
+                              range = 'F13:ER14')
+
+    # set the row name to 'age_group'
+    data$age_group <- age_group
+    data <- tibble::column_to_rownames(data, var = 'age_group')
+
+    return(data)
 
   }
   items <- lapply(X = age_groups, FUN = get_)
 
   # structuring
   readings <- dplyr::bind_rows(items)
-  readings$age_group <- age_groups
-  readings <- tibble::column_to_rownames(readings, var = 'age_group')
 
   # set dates as index
   readings <- as.data.frame(t(readings))
+
+  return(readings)
 
 }
