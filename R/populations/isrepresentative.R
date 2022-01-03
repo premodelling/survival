2039ceec208a5ae80f5a4dfc7071a890d3eb2c8d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-03 01:40:54 +0000

    Add R/populations/isrepresentative.R

---
+++ R/populations/isrepresentative.R
@@ -0,0 +1,29 @@
+# Title     : isrepresentative.R
+# Objective : Determine whether the study demographics are representative of the demographics of the target population
+# Created by: greyhypotheses
+# Created on: 03/01/2022
+
+IsRepresentative <- function () {
+
+
+  # Study
+  distribution <- data %>%
+    select(age_group, sex) %>%
+    group_by(age_group, sex) %>%
+    summarise(N = n(), .groups = 'drop')
+
+  distribution <- tidyr::pivot_wider(data = distribution, id_cols = 'age_group',
+                                     names_from = 'sex', values_from = 'N')
+  distribution <- dplyr::rename(distribution, male = Male, female = Female, unknown = 'NA')
+
+  study <- distribution %>%
+    select(!'unknown') %>%
+    data.frame()
+
+  # Country
+
+
+
+
+
+}
