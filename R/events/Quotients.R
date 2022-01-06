09fdc8e9006a332b43fba352e3770f2a8e95f451
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-06 03:05:53 +0000

    Update R/events/Quotients.R

--- R/events/Quotients.R
+++ R/events/Quotients.R
@@ -4,11 +4,7 @@
 # Created on: 03/01/2022
 
 
-Quotients <- function () {
-
-  source(file = 'R/functions/StudyData.R')
-  data <- StudyData()
-
+QuotientsData <- function (data) {
 
   age_groups <- data %>%
     filter(!is.na(admission_date)& !is.na(age_group)) %>%
@@ -29,14 +25,21 @@ Quotients <- function () {
   demarcations$quotient <- demarcations$N / demarcations$n_by_age_group
   demarcations$age_group <- factor(x = demarcations$age_group,
                                    levels = c('30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90+'),
-                                   ordered = TRUE)
+                                   ordered = TRUE, exclude = NULL)
+  return(demarcations)
 
+}
+
+QuotientsCfAgeGroups <- function (data) {
+
+  demarcations <- QuotientsData(data = data)
 
   demarcations %>%
-    filter(outcome == 'Discharged alive' | outcome == 'Remains in hospital') %>%
+    filter(!is.na(age_group) & !is.na(outcome)) %>%
+    # filter(outcome == 'Discharged alive' | outcome == 'Remains in hospital') %>%
     ggplot(mapping = aes(x = age_group, y = quotient)) +
     geom_bar(alpha = 0.35, stat = 'identity') +
-    facet_wrap(~outcome, scales = 'free') +
+    facet_wrap(~outcome) +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
           panel.grid.minor = element_blank(),
@@ -45,7 +48,29 @@ Quotients <- function () {
           plot.caption = element_text(hjust = 0, size = 11, colour = 'darkgrey'),
           axis.text.x = element_text(size = 11, angle = 90), axis.text.y = element_text(size = 11),
           axis.title.x = element_text(size = 13), axis.title.y = element_text(size = 13)) +
-    ylab(label = '\noutcome/admissions\nper age group\n') +
-    xlab(label = '\ntime to outcome\n')
+    ylab(label = '\n(# of age group outcomes)\n/(# of age group admissions)\n') +
+    xlab(label = '\n')
+
+}
+
+QuotientsCfEvents <- function (data) {
+
+  demarcations <- QuotientsData(data = data)
+
+  demarcations %>%
+    filter(!is.na(age_group)) %>%
+    ggplot(mapping = aes(x = outcome, y = quotient)) +
+    geom_bar(alpha = 0.35, stat = 'identity') +
+    facet_wrap(~age_group) +
+    theme_minimal() +
+    theme(panel.spacing = unit(x = 3, units = 'lines'),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(size = 0.1),
+          strip.text.x = element_text(size = 11, face = 'bold'),
+          plot.caption = element_text(hjust = 0, size = 11, colour = 'darkgrey'),
+          axis.text.x = element_text(size = 11, angle = 90), axis.text.y = element_text(size = 11),
+          axis.title.x = element_text(size = 13), axis.title.y = element_text(size = 13)) +
+    ylab(label = '\nthe outcome proportion of\nage group admissions\n') +
+    xlab(label = '\n')
 
 }
