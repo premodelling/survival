4f7b9cddc005ac9771cf10d6299e7acc7311a1bf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-06 00:57:40 +0000

    Update R/missing/Imputation.R

--- R/missing/Imputation.R
+++ R/missing/Imputation.R
@@ -5,24 +5,12 @@
 
 
 
-#' Imputation: The variables for ...
-#'
-ImputationVariables <- function () {
-  variables <- c('admission_date', 'age_group', 'sex', 'asthma', 'liver_mild', 'renal',
-                 'pulmonary', 'neurological', 'liver_mod_severe', 'malignant_neoplasm',
-                 'outcome', 'time_to_outcome')
-  return(variables)
-}
-
-
-
 #' Imputation: Training Data
 #'
 #' @param training: the training data set
 #'
-ImputationTraining <- function (training) {
-  variables <- ImputationVariables()
-  imputation <- mice::mice(data = training[, variables], m = 13, maxit = 10, seed = 5)
+ImputationTraining <- function (training_) {
+  imputation <- mice::mice(data = training_, m = 13, maxit = 10, seed = 5)
   return(imputation)
 }
 
@@ -32,10 +20,7 @@ ImputationTraining <- function (training) {
 #'
 #' @param testing: The data set for testing
 #'
-ImputationTesting <- function (testing) {
-  variables <- ImputationVariables()
-  keys <- variables[!(variables %in% c('outcome', 'time_to_outcome'))]
-  imputation <- mice::mice(data = testing[, keys], m = 13, maxit = 10, seed = 5)
+ImputationTesting <- function (testing_) {
+  imputation <- mice::mice(data = testing_, m = 13, maxit = 10, seed = 5)
   return(imputation)
 }
-
