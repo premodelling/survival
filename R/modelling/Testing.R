7e90178b4b240db369baedce84957e1e45ad67b2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-09 03:28:22 +0000

    Update R/modelling/Testing.R

--- R/modelling/Testing.R
+++ R/modelling/Testing.R
@@ -3,16 +3,16 @@
 # Created by: greyhypotheses
 # Created on: 06/01/2022
 
-Testing <- function (model_core, model_boosted, testing_)  {
+Testing <- function (model, preliminary)  {
 
   # Core: Narrow external validation
-  external_core <- survival::predict(object = model_core, newdata = testing_)
+  estimates <- survival::predict(object = model, newdata = preliminary)
   # calibration plot
   # AUC
   # sensitivity analysis
 
   # Boosted
-  external_boosted <- mboost::survFit(object = model_boosted, newdata = testing_ )
+  # mboost::survFit(object = model, newdata = preliminary )
   # calibration plot
   # AUC
   # sensitivity analysis
