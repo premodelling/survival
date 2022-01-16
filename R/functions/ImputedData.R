117dc1c0a254a52e9422ab5451d6357348abd646
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-16 21:03:20 +0000

    Add R/functions/ImputedData.R

---
+++ R/functions/ImputedData.R
@@ -0,0 +1,44 @@
+# Title     : ImputedData.R
+# Objective : Imputed Data
+# Created by: greyhypotheses
+# Created on: 16/01/2022
+
+source(file = 'R/functions/StudyData.R')
+source(file = 'R/functions/TemporalSplit.R')
+source(file = 'R/missing/ImputationStep.R')
+
+ImputedData <- function(upload = TRUE) {
+
+  # The data set
+  data <- StudyData()
+
+
+  # Splitting
+  dataframes <- TemporalSplit(data = data)
+  training <- dataframes$training
+  testing <- dataframes$testing
+  rm(dataframes)
+
+
+  # all variables for training phase
+  variables <- c('admission_date', 'age_group', 'sex', 'asthma', 'liver_mild', 'renal',
+                 'pulmonary', 'neurological', 'liver_mod_severe', 'malignant_neoplasm',
+                 'outcome', 'time_to_outcome')
+
+
+  # imputation
+  #
+  #
+  # ...  re-calculate: censored & outcome_date, and add deceased
+  # ...  the imputation variables exclude: outcome_date (date can't be used
+  #      in the MICE models, time_to _outcome in lieu), censored (it is a project
+  #      variable created for analysis & graphing purposes)
+  training_ <- ImputationStep(initial = training[, variables],
+                              phase = 'training', upload = upload)
+
+  testing_ <- ImputationStep(initial = testing[, variables],
+                             phase = 'testing', upload = upload)
+
+  return(list(data = data, training_ = training_, testing_ = testing_))
+
+}
