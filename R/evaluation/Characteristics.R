308270731862fdcfbd2b615fd348223383c9c473
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-18 22:03:25 +0000

    Update R/evaluation/Characteristics.R

--- R/evaluation/Characteristics.R
+++ R/evaluation/Characteristics.R
@@ -14,27 +14,21 @@ Characteristics <- function () {
   rbind(deaths, round(prop.table(deaths), digits = 3))
 
   # Demographics
-  demographics <- table(data$sex, data$age_group, useNA = 'always')
-  t(demographics)
-  t(round(prop.table(demographics), digits = 3))
-  brief <- data %>%
+  demographics <- data %>%
     select(sex, age_group) %>%
     group_by(sex, age_group) %>%
     summarise(N = n()) %>%
     pivot_wider(id_cols = age_group, names_from = sex, values_from = N) %>%
     data.frame()
-  brief
+  demographics
 
   # Diseases
-  diseases <- c('asthma', 'liver_mild', 'renal', 'pulmonary', 'neurological', 'liver_mod_severe', 'malignant_neoplasm')
-  frequencies <- function (field) {
-    N <- table(data[, field], useNA = 'always')
-    fractions <- round(prop.table(N), digits = 2)
-    frame <- data.frame(rbind(N, fractions))
-    row.names(frame) <- c(field, 'fractions')
-    return(frame)
-  }
-  details <- lapply(X = diseases, FUN = frequencies)
-  dplyr::bind_rows(details)
+  diseases <- data %>%
+    select(asthma, liver_mild, renal, pulmonary, neurological, liver_mod_severe, malignant_neoplasm) %>%
+    gather(key = 'disease', value = 'Class') %>%
+    group_by(disease, Class) %>%
+    summarise(N = n(), .groups = 'drop') %>%
+    pivot_wider(id_cols = 'disease', names_from = 'Class', values_from = 'N') %>%
+    data.frame()
 
 }
