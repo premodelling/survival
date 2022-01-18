8c1cd6df6223bd0167551c87dfb4ab0e4dd7249d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-18 22:42:50 +0000

    Update R/background/Numbers.R

--- R/background/Numbers.R
+++ R/background/Numbers.R
@@ -1,28 +1,45 @@
 # Title     : Numbers.R
-# Objective : Characteristics
+# Objective : Numbers
 # Created by: greyhypotheses
 # Created on: 16/01/2022
 
-source(file = '../functions/StudyData.R')
 
-Characteristics <- function () {
+#' Deaths
+DeceasedNumbers <- function (data) {
 
-  data <- StudyData()
-
-  # Mortality
   deaths <- table(data$deceased, useNA = 'always')
-  rbind(deaths, round(prop.table(deaths), digits = 3))
+  return(data.frame(yes = deaths['1'],
+                    no = deaths['0'],
+                    unknown = deaths[length(deaths)],
+                    row.names = 'deceased'))
+}
 
-  # Demographics
+#' Demographics
+DemographicsNumbers <- function (data) {
+
   demographics <- data %>%
     select(sex, age_group) %>%
     group_by(sex, age_group) %>%
-    summarise(N = n()) %>%
+    summarise(N = n(), .groups = 'drop') %>%
     pivot_wider(id_cols = age_group, names_from = sex, values_from = N) %>%
     data.frame()
-  demographics
 
-  # Diseases
+  demographics$Total <- select(demographics, !age_group) %>% rowSums()
+  names(demographics) <- c('Age Group', 'Male', 'Female', 'Unknown', 'Total')
+
+  return(demographics)
+
+}
+
+#' Diseases
+DiseaseNumbers <- function (data) {
+
+  disease <- c('asthma', 'liver_mild', 'liver_mod_severe',
+            'malignant_neoplasm', 'neurological', 'pulmonary', 'renal')
+  name <- c('Asthma', 'Mild Liver Disease', 'Moderate, Severe Liver Disease',
+            'Malignant Neoplasm', 'Neurological Disorder', 'Pulmonary Disease', 'Renal Disease')
+  labels <- data.frame(disease = disease, Comorbidity = name)
+
   diseases <- data %>%
     select(asthma, liver_mild, renal, pulmonary, neurological, liver_mod_severe, malignant_neoplasm) %>%
     gather(key = 'disease', value = 'Class') %>%
@@ -30,5 +47,15 @@ Characteristics <- function () {
     summarise(N = n(), .groups = 'drop') %>%
     pivot_wider(id_cols = 'disease', names_from = 'Class', values_from = 'N') %>%
     data.frame()
+  diseases$Total <- select(diseases, !disease) %>% rowSums()
+  names(diseases) <- c('disease', 'No', 'Yes', 'Unknown', 'Total')
+
+  diseases <- dplyr::left_join(diseases, labels, by = 'disease') %>%
+    select(Comorbidity, No, Yes, Unknown, Total)
+
+  return(diseases)
 
 }
+
+
+
