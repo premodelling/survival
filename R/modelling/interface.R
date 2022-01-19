17d94704963ac80e8e893cca2f6ec8b7353191db
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-19 13:10:56 +0000

    Update R/modelling/interface.R

--- R/modelling/interface.R
+++ R/modelling/interface.R
@@ -19,28 +19,23 @@ source(file = 'R/evaluation/SurvivalCurve.R')
 dataframes <- ExtensiveStudyData(upload = TRUE)
 data <- dataframes$data
 training_ <- dataframes$training_
-testing_ <- dataframes$testing_
 data_ <- dataframes$data_
-
-even <- data[complete.cases(data),]
-
-source(file = 'R/modelling/Univariate.R')
-analysis <- Univariate(blob = training_)
-analysis <- Univariate(blob = data_)
-analysis <- Univariate(blob = even)
 
 
 source(file = 'R/modelling/Multivariate.R')
 analysis <- Multivariate(blob = training_)
 analysis <- Multivariate(blob = data_)
-analysis <- Multivariate(blob = even)
+analysis <- Multivariate(blob = data[complete.cases(data), ])
+
+diagnostics <- analysis$diagnostics
+model <- analysis$model
 
 
 #' Training
 #'
 #' The Cox model is inappropriate, proportionality assumption does not hold.
 #'
-#' For time depedent cases:
+#' For time dependent cases:
 #' https://www.emilyzabor.com/tutorials/survival_analysis_in_r_tutorial.html#Time-dependent_covariate
 #' https://glmnet.stanford.edu/articles/Coxnet.html#cox-models-for-start-stop-data-1
 #' https://www.rdocumentation.org/packages/glmnet/versions/4.1-3/topics/glmnet
@@ -51,8 +46,8 @@ analysis <- Multivariate(blob = even)
 
 SurvivalCurve(data = training_, caption = 'Training Data (multiply imputed)')
 ViolationsKaplan(data = training_)
-ViolationsTime(model = unboosted)
-ViolationsProportionalityTable(model = unboosted)
+ViolationsTime(model = model)
+ViolationsProportionalityTable(model = model)
 
 
 
