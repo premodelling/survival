2cdc7b2cb81c449e608dee381c5d118b78983434
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-20 19:39:40 +0000

    Update R/background/Numbers.R

--- R/background/Numbers.R
+++ R/background/Numbers.R
@@ -8,15 +8,25 @@
 DeceasedNumbers <- function (data) {
 
   deaths <- table(data$deceased, useNA = 'always')
-  return(data.frame(yes = deaths['1'],
-                    no = deaths['0'],
-                    unknown = deaths[length(deaths)],
-                    row.names = 'deceased'))
+  deceased <- data.frame(variable = 'Deceased', yes = deaths['1'], no = deaths['0'],
+                         unknown = deaths[length(deaths)], total = sum(deaths))
+
+  deceased <- deceased %>%
+    mutate(yes_ = yes/total, no_ = no/total, unknown_ = unknown/total)
+
+  deceased <- deceased %>%
+    select(variable, yes, no, unknown, yes_, no_, unknown_)
+
+  return(deceased)
+
+
 }
+
 
 #' Demographics
 DemographicsNumbers <- function (data) {
 
+  # numbers
   demographics <- data %>%
     select(sex, age_group) %>%
     group_by(sex, age_group) %>%
@@ -24,21 +34,29 @@ DemographicsNumbers <- function (data) {
     pivot_wider(id_cols = age_group, names_from = sex, values_from = N) %>%
     data.frame()
 
+  # female ?
   demographics$Total <- select(demographics, !age_group) %>% rowSums()
-  names(demographics) <- c('Age Group', 'Male', 'Female', 'Unknown', 'Total')
+  names(demographics) <- c('variable', 'no', 'yes', 'unknown', 'total')
+
+  # percentages
+  demographics <- demographics %>%
+    mutate(no_ = no/total, yes_ = yes/total, unknown_ = unknown/total)
+  demographics <- demographics %>%
+    select(variable, yes, no, unknown, yes_, no_, unknown_)
 
   return(demographics)
 
 }
+
 
 #' Diseases
 DiseaseNumbers <- function (data) {
 
   disease <- c('asthma', 'liver_mild', 'liver_mod_severe',
             'malignant_neoplasm', 'neurological', 'pulmonary', 'renal')
-  name <- c('Asthma', 'Mild Liver Disease', 'Moderate, Severe Liver Disease',
-            'Malignant Neoplasm', 'Neurological Disorder', 'Pulmonary Disease', 'Renal Disease')
-  labels <- data.frame(disease = disease, Comorbidity = name)
+  name <- c('Asthma', 'Mild Liver\nDisease', 'Moderate, Severe\nLiver Disease',
+            'Malignant Neoplasm', 'Neurological\nDisorder', 'Pulmonary Disease', 'Renal Disease')
+  labels <- data.frame(disease = disease, variable = name)
 
   diseases <- data %>%
     select(asthma, liver_mild, renal, pulmonary, neurological, liver_mod_severe, malignant_neoplasm) %>%
@@ -48,14 +66,43 @@ DiseaseNumbers <- function (data) {
     pivot_wider(id_cols = 'disease', names_from = 'Class', values_from = 'N') %>%
     data.frame()
   diseases$Total <- select(diseases, !disease) %>% rowSums()
-  names(diseases) <- c('disease', 'No', 'Yes', 'Unknown', 'Total')
+  names(diseases) <- c('disease', 'no', 'yes', 'unknown', 'total')
 
   diseases <- dplyr::left_join(diseases, labels, by = 'disease') %>%
-    select(Comorbidity, No, Yes, Unknown, Total)
+    select(variable, no, yes, unknown, total)
+
+  diseases <- diseases %>%
+    mutate(yes_ = yes/total, no_ = no/total, unknown_ = unknown/total)
+
+  diseases <- diseases %>%
+    select(variable, yes, no, unknown, yes_, no_, unknown_)
 
   return(diseases)
 
 }
 
+
+#' A summary of the calculations above
+SpecialNumbers <- function (data) {
+
+  blank <- data.frame(variable = '', yes = NA, no = NA, unknown = NA, yes_ = NA, no_ = NA, unknown_ = NA)
+
+  deceased <- DeceasedNumbers(data = data)
+  deceased_ <- data.frame(variable = 'DEATHS', yes = NA, no = NA, unknown = NA,
+                          yes_ = NA, no_ = NA, unknown_ = NA)
+
+  diseases <- DiseaseNumbers(data = data)
+  diseases_ <- data.frame(variable = 'COMORBIDITIES', yes = NA, no = NA, unknown = NA,
+                          yes_ = NA, no_ = NA, unknown_ = NA)
+
+  demographics <- DemographicsNumbers(data = data)
+  demographics_ <- data.frame(variable = 'AGE GROUP, FEMALE', yes = NA, no = NA, unknown = NA,
+                              yes_ = NA, no_ = NA, unknown_ = NA)
+
+  return(rbind(deceased_, deceased, blank,
+               diseases_, diseases, blank,
+               demographics_, demographics, blank))
+
+}
 
 
