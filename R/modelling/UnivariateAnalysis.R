845a08687fbd02bde460ac103f24f15503c80509
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-10-21 22:57:10 +0100

    Update R/modelling/UnivariateAnalysis.R

--- R/modelling/UnivariateAnalysis.R
+++ R/modelling/UnivariateAnalysis.R
@@ -46,6 +46,25 @@ UnivariateAnalysisData <- function () {
   analysis <- Univariate(blob = data_)
   diagnostics <- analysis$diagnostics
 
+  # names, labels, indices
+  diagnostics$label <- row.names(diagnostics)
+  row.names(diagnostics) <- NULL
+  diagnostics <- diagnostics %>% arrange(label)
+
+  settings <- data.frame(label = c('age_group70-79', 'age_group60-69', 'age_group50-59', 'age_group90+', 'age_group40-49',
+                                   'age_group30-39', 'sexFemale', 'asthmayes', 'liver_mildyes', 'renalyes', 'pulmonaryyes',
+                                   'neurologicalyes', 'liver_mod_severeyes', 'malignant_neoplasmyes'),
+                         name = c('Age Group 70 - 79', 'Age Group 60 - 69', 'Age Group 50 - 59', 'Age Group 90+',
+                                  'Age Group 40 - 49', 'Age Group 30 - 39', 'Sex Female', 'Asthma Yes',
+                                  'Liver Disease, (Mild) Yes', 'Renal Disease Yes', 'Pulmonary Disease Yes',
+                                  'Neurological Disorder Yes', 'Liver Disease, (Moderate, severe) Yes', 'Malignant Neoplasm Yes'))
+
+  diagnostics <- dplyr::left_join(x = diagnostics, y = settings, by = 'label')
+  diagnostics <- diagnostics %>%
+    select(name, Coefficient, HR, HRLCI, HRUCI, p_value, proportionality)
+
+  return(diagnostics)
+
 
 
 }
@@ -58,6 +77,26 @@ UnivariateAnalysisCC <- function () {
 
   C <- data[complete.cases(data),]
   analysis <- Univariate(blob = C)
+  diagnostics <- analysis$diagnostics
+
+  # names, labels, indices
+  diagnostics$label <- row.names(diagnostics)
+  row.names(diagnostics) <- NULL
+  diagnostics <- diagnostics %>% arrange(label)
+
+  settings <- data.frame(label = c('age_group70-79', 'age_group60-69', 'age_group50-59', 'age_group90+', 'age_group40-49',
+                                   'age_group30-39', 'sexFemale', 'asthmayes', 'liver_mildyes', 'renalyes', 'pulmonaryyes',
+                                   'neurologicalyes', 'liver_mod_severeyes', 'malignant_neoplasmyes'),
+                         name = c('Age Group 70 - 79', 'Age Group 60 - 69', 'Age Group 50 - 59', 'Age Group 90+',
+                                  'Age Group 40 - 49', 'Age Group 30 - 39', 'Sex Female', 'Asthma Yes',
+                                  'Liver Disease, (Mild) Yes', 'Renal Disease Yes', 'Pulmonary Disease Yes',
+                                  'Neurological Disorder Yes', 'Liver Disease, (Moderate, severe) Yes', 'Malignant Neoplasm Yes'))
+
+  diagnostics <- dplyr::left_join(x = diagnostics, y = settings, by = 'label')
+  diagnostics <- diagnostics %>%
+    select(name, Coefficient, HR, HRLCI, HRUCI, p_value, proportionality)
+
+  return(diagnostics)
 }
 
 
